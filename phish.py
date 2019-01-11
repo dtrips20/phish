f1e37f783c6650da6e9bdc1e76de70912c7d95f6
dtrips <deepaktripathi20@gmail.com>
2019-01-10 23:06:31 -0800

    Updated to fetch URLs from PhishTank.

--- phish.py
+++ phish.py
@@ -1,62 +1,65 @@
-import json
 import os
 import time
 import feedparser
 import ijson
 import requests
 import urllib.request
+import shutil
+import bz2
 
 api_key = "9bfbd8e16dd87bda0a598ee964db349bdace48fc70b126e3362a3c581bbb1aeb"
 url = 'http://data.phishtank.com/data/{0}/online-valid.json.bz2'.format(api_key)
 
 
-def parse_json():
+def parse_json(file_name):
 
     cnt = 0
     t0 = time.time()
-    with open('C:/Users/dtrips/Downloads/verified_online.json/verified_online.json', 'r') as f:
+    with open(file_name, 'r') as f:
         parse = ijson.parse(f)
         for prefix, event, value in parse:
             if prefix == 'item.url':
                 # print('prefix={}, event={}, value={}'.format(prefix, event, value))
-                # url = value
+                url = value
                 # m=hashlib.sha256(url.encode())
-                # print ( url , m.hexdigest())
+                print(url)#, m.hexdigest())
                 cnt = cnt + 1
     t1 = time.time()
     print(cnt)
-    print(t0 - t1)
+    print(t1 - t0)
 
 
 def etag_changed():
 
     file_name = os.path.join(os.getcwd(), 'etag.txt')
-    print(file_name)
 
     if not os.path.exists(file_name):
         print('File does not exits so create a new file')
         open(file_name, 'a').close()
 
     if os.path.getsize(file_name) > 0:
-        print('file size is greater than 0')
         f = open(file_name, 'r')
         if f.mode == 'r':
             contents = f.read()
-            print(contents)
             f.close()
+
+            # HEAD request to PhishTank
             request = requests.head(url)
-            print(request.headers['Location'])
-            cloud_url = request.headers['Location']
-            cloud_request = requests.head(cloud_url)
-            print(cloud_request.headers)
-            print(cloud_request.headers['ETag'])
-            f = open(file_name, 'w+')
-            f.write(cloud_request.headers['ETag'])
-            f.close()
+            cloud_url_location = request.headers['Location']
+
+            # HEAD request to cloud url to check changed ETag
+            cloud_request = requests.head(cloud_url_location)
+            print("Saved ETag :", contents)
+            print("Current ETag :", cloud_request.headers['ETag'])
+
             if contents == cloud_request.headers['ETag']:
-                return False
+                return '', False
             else:
-                return True
+                # save the ETag
+                f = open(file_name, 'w+')
+                f.write(cloud_request.headers['ETag'])
+                f.close()
+                return cloud_url_location, True
     else:
         print('file size is not greater than 0')
         d2 = feedparser.parse(url)
@@ -66,31 +69,26 @@ def etag_changed():
         f.close()
 
 
-def download_bz_file():
+def download_bz_file_and_decompress(cloud_url):
 
-    file_name = url.split('/')[-1]
-    u = urllib.request.urlretrieve(url, 'online-valid.json.bz2')
-    f = open(file_name, 'wb')
-    meta = u.info()
-    file_size = int(meta.getheaders("Content-Length")[0])
-    print("Downloading: %s Bytes: %s" % (file_name, file_size))
+    new_file_path = ''
+    file_name = (cloud_url.split('/')[-1]).split('?')[0]
+    # u = urllib.request.urlretrieve(url, file_name)
+    with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+        shutil.copyfileobj(response, out_file)
 
-    file_size_dl = 0
-    block_sz = 8192
-    while True:
-        buffer = u.read(block_sz)
-        if not buffer:
-            break
+    stat_info = os.stat(file_name)
 
-        file_size_dl += len(buffer)
-        f.write(buffer)
-        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-        status = status + chr(8) * (len(status) + 1)
-        print(status,)
+    if stat_info.st_size > 0:
+        print(stat_info.st_size)
+        # unzip the json feed
+        zipfile = bz2.BZ2File(file_name)  # open the file
+        data = zipfile.read()  # get the decompressed data
+        new_file_path = file_name[:-4]  # assuming the filepath ends with .bz2
+        open(new_file_path, 'wb').write(data)  # write a uncompressed file
 
-    f.close()
-# resp = requests.head(url)
-# print (resp.history , resp.text, resp.headers)
+    return new_file_path
+
 
 
 '''
@@ -109,7 +107,7 @@ sql = "INSERT into urls (url,sha256 , label , added_date) values ( %s , %s, %s, 
 
 '''
 
-value = json.loads(open("C:/Users/dtrips/Downloads/verified_online.json/verified_online.json").read())
+#value = json.loads(open("C:/Users/dtrips/Downloads/verified_online.json/verified_online.json").read())
 
 '''
 for url in urls:
@@ -126,9 +124,12 @@ db.close()
 print("Record inserted")
 '''
 if __name__ == '__main__':
-    download_bz_file()
-    if etag_changed():
+
+    cloud_url_loc, etag_has_changed = etag_changed()
+
+    if etag_has_changed:
         print("ETag has changed")
-        print("Parse the JSON")
+        new_file = download_bz_file_and_decompress(cloud_url_loc)
+        parse_json(new_file)
     else:
-        print("Dont parse the json")
+        print("Dont parse the json as ETag has not changed")
