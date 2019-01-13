a12fc64ebfe9c08d6d4d020d19062a2d45aef3cd
dtrips <deepaktripathi20@gmail.com>
2019-01-12 16:37:21 -0800

    Updated / Fixed lexical features.

--- lexical_features.py
+++ lexical_features.py
@@ -4,134 +4,114 @@ Created on Sat Dec 15 17:41:08 2018
 @author: deepak tripathi
 
 """
-# import mysql.connector
+
 import ipaddress as ip
 from urllib.parse import urlparse
 import tldextract
-import whois
-from os.path import splitext
+# import whois
+# from os.path import splitext
 from config import read_shady_tlds_config
 from config import read_shorten_url_services_config
 
 
-class LexicalFeature:
-
+class LexFeature:
     _url_to_compute = None
-    _shady_tlds = read_shady_tlds_config()
-    _shorten_url_services = read_shorten_url_services_config()
+    _shady_tlds = None
+    _shorten_url_services = None
 
-    def __init__(self, url_to_compute ):
+    def __init__(self, url_to_compute):
         self._url_to_compute = url_to_compute
+        self._shady_tlds = read_shady_tlds_config()
+        self._shorten_url_services = read_shorten_url_services_config()
 
-    # Feature 1 : Method to count number of dots
+    # Feature 1: Method to count number of dots
     def count_dots(self):
         return self._url_to_compute.count('.')
 
-    # Feature 2 : Method to count number of delimeters
-    def count_delim(self):
+    # Feature 2: Method to count number of delimiters
+    def count_delimiters(self):
         count = 0
-        delim = [';', '_', '?', '=', '&']
+        delimiters = [';', '_', '?', '=', '&']
         for each in self._url_to_compute:
-            if each in delim:
+            if each in delimiters:
                 count = count + 1
 
         return count
 
-    # Feature 3 :Is IP addr present as th hostname, let's validate
-    def is_ip(self):
+    # Feature 3: Is IP addr present as th hostname, let's validate
+    def is_domain_ip(self):
+
         try:
-            if ip.ip_address(self._url_to_compute):
+            ext = tldextract.extract(self._url_to_compute)
+            ip_address = ip.ip_address(ext.domain)
+            if ip_address:
                 return 1
-        except:
+        except Exception as e:
             return 0
 
     # Feature 4: method to check the presence of hyphens
     def is_present_hyphen(self):
         return self._url_to_compute.count('-')
 
-    # Feature 5 : count number of sub-directories
+    # Feature 5: count number of sub-directories
     def count_sub_dir(self):
         return self._url_to_compute.count('/')
 
-    # Feature 6: Return the filename extension from url.
-    def get_ext(self):
-        root, ext = splitext(self._url_to_compute)
-        return ext
-
-    # Feature 7 : count sub domains
+    # Feature 6: count sub domains
     def count_sub_domain(self):
 
+        extract = tldextract.extract(self._url_to_compute)
+        sub_domain = extract.subdomain
 
         if not sub_domain:
             return 0
         else:
             return len(sub_domain.split('.'))
 
+    # Feature 7: count queries
+    def count_queries(self):
 
-    # Feature 8 : count queries
-    def count_queries(query):
-        if not query:
+        parsed_url = urlparse(self._url_to_compute)
+
+        if not parsed_url.query:
             return 0
         else:
-            return len(query.split('&'))
+            return len(parsed_url.query.split('&'))
 
+    # Feature 8: Presence of shady domain
+    def shady_tld(self):
 
-    # Feature 9 : Presense of shady domain
-    def shady_tld(suffix):
-        if suffix in shady_tlds:
+        extract = tldextract.extract(self._url_to_compute)
+
+        if extract.suffix in self._shady_tlds:
             return 1
         else:
             return 0
 
-
-    # Feature 10 : Long URL to hide the suspicious part
+    # Feature 9: Long URL to hide the suspicious part
     # Rule
     #        if url length <= 75 then feature is legit:
     #            else if url length > 75 then its phishing
-    def is_suspicious_part_hidden(url):
-        if len(url) <= 75:
+    def is_suspicious_part_hidden(self):
+        if len(self._url_to_compute) <= 75:
             return 0
         else:
             return 1
 
-
-    # Feature 11 : Using URL shortening services "Tiny URL"
+    # Feature 10: Using URL shortening services "Tiny URL"
     # Rule
     #   if tiny url then phishing :
     #      else legitimate
-    def is_tiny_url(url):
-        if url in shorten_url_services:
+    def is_tiny_url(self):
+
+        parsed_url = urlparse(self._url_to_compute)
+
+        if parsed_url.netloc in self._shorten_url_services:
             return 1
         else:
             return 0
 
-
-    # Feature 12 : URL's having "@" symbol
+    # Feature 11: URL's having "@" symbol
     # Using @ symbol
 
     # end of features , start saving it.
-
-    if __name__ == '__main__':
-
-        url = "http://www.google.com"
-        path = urlparse(url)
-        print("Path :" + str(path))
-        ext = tldextract.extract(url)
-
-        domain = '.'.join(ext[1:])
-
-        try:
-            w = whois.query(domain)
-        except:
-            print("Unknow TDL")
-        # print (w.__dict__)
-
-        print("Number of dots :" + str(count_dots()))
-        print("Number of delim :" + str(count_delim(url)))
-        print("Is IP :" + str(is_ip(ext.domain)))
-        print("Is hyphen present :" + str(is_present_hyphen(path.netloc)))
-        print("Count sub directory :" + str(count_sub_dir(url)))
-        print("Count queries :" + str(count_queries(path.query)))
-        print(ext.suffix + " - Supicious Shady top level domain :" + str(shady_tld(ext.suffix)))
-        print("is suspicious part hidden :" + str(is_suspicious_part_hidden(url)))
-        print("Shorted url present :" + str(is_tiny_url(path.netloc)))
