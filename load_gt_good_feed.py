c23c40344af9a726fec9415c72532e1907b8440a
dtrips <deepaktripathi20@gmail.com>
2019-01-12 21:36:19 -0800

    Update code for GT good feed.

--- load_gt_good_feed.py
+++ load_gt_good_feed.py
@@ -16,9 +16,9 @@ from bs4 import BeautifulSoup
 import hashlib
 from datetime import datetime
 from mysql_connect import MysqlPython
+import labeler
 
-
-phishTankLegitUrls="https://www.phishtank.com/phish_search.php?page=4&valid=n&Search=Search"
+phishTankLegitUrls = "https://www.phishtank.com/phish_search.php?page=1&valid=n&Search=Search"
 
 # create a fake user agent so it looks like a real query.
 headers = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -33,7 +33,7 @@ def simple_get(url):
     """
     try:
 
-        with closing(get(url,headers=headers, stream=True)) as resp:
+        with closing(get(url, headers=headers, stream=True)) as resp:
             if is_good_response(resp):
                 return resp.content
             else:
@@ -49,8 +49,8 @@ def is_good_response(resp):
     Returns True if the response seems to be HTML, False otherwise.
     """
     content_type = resp.headers['Content-Type'].lower()
-    return (resp.status_code == 200 
-            and content_type is not None 
+    return (resp.status_code == 200
+            and content_type is not None
             and content_type.find('html') > -1)
 
 
@@ -61,24 +61,32 @@ def log_error(e):
     make it do anything.
     """
     print(e)
-        
+
 
 if __name__ == "__main__":
     raw_html = simple_get(phishTankLegitUrls)
     # print("Length of raw html " + str(len(raw_html)))
     html = BeautifulSoup(raw_html, 'html.parser')
     connect_mysql = MysqlPython()
-    insertedRecords = 0 
+    insertedRecords = 0
     sql = "INSERT into urls (url,sha256 , label , added_date) values ( %s , %s, %s, %s)"
 
     for a in html.select('a'):
-        if "phish_detail.php"  in a['href']:
-            #print(a['href'])
-            raw_html = simple_get("https://www.phishtank.com/"+a['href'])
+        if "phish_detail.php" in a['href']:
+            # print(a['href'])
+            raw_html = simple_get("https://www.phishtank.com/" + a['href'])
             html = BeautifulSoup(raw_html, 'html.parser')
             for a in html.select('a'):
                 if 'visit the site' in a.text:
                     url = a['href']
                     m = hashlib.sha256(url.encode())
-                    connect_mysql.insert('urls',url=url, source='Legit',sha256=m.hexdigest(),label=0,added_date=datetime.utcnow())
+                    conditional_query = 'sha256 = %s'
+                    items = connect_mysql.select("urls", conditional_query, "id", sha256=m.hexdigest())
+                    if items:
+                        print("Don't insert the values ", m.hexdigest())
+                        found = True
+                    else:
+                        connect_mysql.insert('urls', url=url, source='Legit', sha256=m.hexdigest(), label=0,
+                                             added_date=datetime.utcnow())
+                        labeler.save_features(url, m.hexdigest(), 0)
 print("Record inserted")
